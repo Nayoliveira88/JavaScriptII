0dafbd4840778ba9719bf3275fe859e7741c204b
Nayane Maciel <naymoliveira88@gmail.com>
2021-12-15 21:02:10 -0300

    Alterações da segunda aula

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function carregamentoPagina( ) {
-    alert("Mensagem apresentada no carregamento do sistema");
-}
+// function carregamentoPagina( ) {
+//     alert("Mensagem apresentada no carregamento do sistema");
+// }
+
+$(() => {
+    //código executado no carregamento da pagina
+    // alert("Mensagem apresentada no carregamento do sistema");
+
+    $("#btnSalvar").click(() => {
+        console.log("FUNÇÃO CLICK FUNCIONOU")
+    })
+})
